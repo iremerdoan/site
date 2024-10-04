57ea9edea43c4caf40f7f4f01d70fd15ca78eba2
iremerdoan <lalairemm@gmail.com>
2024-10-04 12:44:49 +0300

    ikinci yukleme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./ana/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: { // Corrected from 'container{' to 'container: {'
+      center: true, // Added a comma here
+      screens: { // Corrected from 'screen{' to 'screens: {'
+        lg: '1148px',
+        xl: '1148px',
+        '2xl': '1148px'
+      } // Closing brace for screens
+    }, // Closing brace for container
+    extend: {
+      fontFamily: {
+        playwrite: ["Playwrite DE Grund", 'cursive'],
+        edu: ["Edu AU VIC WA NT Guides", 'cursive']
+      }
+    } // Closing brace for extend
   },
-  plugins: [],
-}
+  plugins: [] // Added a comma here
+};
